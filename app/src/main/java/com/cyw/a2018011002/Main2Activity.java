1e2d3377bec5ea0ba5148056a3f920709b25eeac
totorowg <wangchingying@gmail.com>
2018-01-10 11:45:24 +0800

    Main2Activity要記得get Intent

--- app/src/main/java/com/cyw/a2018011002/Main2Activity.java
+++ app/src/main/java/com/cyw/a2018011002/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.cyw.a2018011002;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Intent it = getIntent();
+        Bundle b = it.getExtras();
+        String str = b.getString("DATA");
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(str);
     }
+
 }
